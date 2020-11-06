6b2024cff49572726fa4f3f3c55ca97be83ce57a
MARAIS Lucas <Lucas.Marais@bordeaux-inp.fr>
2020-11-06 17:52:55 +0100

    color.cv2

--- latest/src/color.c
+++ latest/src/color.c
@@ -1,1 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+
+#define COLOR_NUMBER 3
+
+struct color{
+  char *ANSI;
+  char *name;
+};
+
+struct color red = {"\e[0;31m","rouge"};
+struct color blue = {"\e[0;34m","bleu"};
+struct color green = {"\e[0;32m","vert"};
+
+struct color* colors[COLOR_NUMBER] = [&red, &blue, &green];
+
+const char* color_name(struct color* t){
+  return t->name;
+}
+
+const char* color_cstring(struct color* t) {
+  return t->ANSI;
+}
+
+struct color* color_from_name(const char* name)
+{
+  for (int i = 0; i < COLOR_NUMBER; ++i) {
+    if (strcmp(colors[i]->name,name) == 0)
+      return colors[i];
+  }
+  return NULL;
+}
 
--- latest/src/tile.c
+++ latest/src/tile.c
@@ -1,1 +1,44 @@
 #include "color.h"
+
+enum direction { NORTH, SOUTH, EAST, WEST,
+  MAX_DIRECTION, ERROR_DIRECTION = -1 };
+
+struct tile {
+  struct color* colored_tile[4];
+};
+
+#define MAX_DECK_SIZE 100
+
+struct deck_pair {
+  const struct tile* t;
+  unsigned int n;
+};
+
+struct deck {
+  struct deck_pair cards[MAX_DECK_SIZE];
+  unsigned int size;
+};
+
+// A tile that is empty
+const struct tile* empty_tile()
+{
+  return NULL
+}
+
+// A predicate telling if the tile is empty : return 1 if the tile is empty and 0 otherwise
+int tile_is_empty(const struct tile* t)
+{
+  return (t == NULL);
+}
+
+// A comparator between tiles
+int tile_equals(const struct tile* t1, const struct tile* t2)
+{
+  
+}
+
+// Accessors to the color of the edges
+struct color* tile_edge(const struct tile* t, enum direction d);
+
+// A function that fills a deck with tiles
+void deck_init(struct deck* d);
