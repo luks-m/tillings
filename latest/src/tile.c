abdbe0298348a978374c390ddc73f946e492b2b2
MARAIS Lucas <Lucas.Marais@bordeaux-inp.fr>
2020-11-10 18:13:41 +0100

    test sans tile.c

--- latest/src/tile.c
+++ latest/src/tile.c
@@ -9,7 +9,7 @@ struct tile {
 // A tile that is empty
 const struct tile* empty_tile()
 {
-	return NULL;
+  return NULL;
 }
 
 // A predicate telling if the tile is empty : return 1 if the tile is empty and 0 otherwise
@@ -21,61 +21,61 @@ int tile_is_empty(const struct tile* t)
 // A comparator between tiles
 int tile_equals(const struct tile* t1, const struct tile* t2)
 {
-	for (int i = 0 ; i < 4 ; ++i){
-		if (t1->colored_tile[i] != t2 ->colored_tile[i])
-			return 0;		
-	}
-	return 1;
+  for (int i = 0 ; i < 4 ; ++i){
+    if (t1->colored_tile[i] != t2 ->colored_tile[i])
+      return 0;		
+  }
+  return 1;
 }
 
 // Accessors to the color of the edges
 struct color* tile_edge(const struct tile* t, enum direction d)
 {
-	return t->colored_tile[d];
+  return t->colored_tile[d];
 }
-
+/*
 struct color* red_p = color_from_name("rouge");
 struct color* blue_p = color_from_name("bleu");
 struct color* green_p = color_from_name("vert");
 
 struct tile tiles[MAX_DECK_SIZE] = {
-									{red_p, red_p, red_p, red_p},
-									{blue_p, blue_p, blue_p, blue_p},
-									{green_p, green_p, green_p, green_p},
-									{green_p, green_p, red_p, red_p},
-									{blue_p, blue_p, red_p, red_p},
-									{green_p, green_p, blue_p, blue_p},
-									{red_p, green_p, green_p, red_p},
-									{blue_p, red_p, red_p, blue_p},
-									{green_p, blue_p, blue_p, green_p},
-									{blue_p, green_p, red_p, green_p},
-									{red_p, blue_p, green_p, blue_p},
-									{green_p, red_p, blue_p, red_p},
-									{green_p, blue_p, red_p, red_p},
-									{red_p, green_p, blue_p, blue_p},
-									{blue_p, red_p, green_p, green_p},
-									{red_p, blue_p, blue_p, blue_p},
-									{red_p, green_p, green_p, green_p},
-									{green_p, red_p, red_p, red_p},
-									{green_p, blue_p, blue_p, blue_p},
-									{blue_p, red_p, red_p, red_p}
+				    {red_p, red_p, red_p, red_p},
+				    {blue_p, blue_p, blue_p, blue_p},
+				    {green_p, green_p, green_p, green_p},
+				    {green_p, green_p, red_p, red_p},
+				    {blue_p, blue_p, red_p, red_p},
+				    {green_p, green_p, blue_p, blue_p},
+				    {red_p, green_p, green_p, red_p},
+				    {blue_p, red_p, red_p, blue_p},
+				    {green_p, blue_p, blue_p, green_p},
+				    {blue_p, green_p, red_p, green_p},
+				    {red_p, blue_p, green_p, blue_p},
+				    {green_p, red_p, blue_p, red_p},
+				    {green_p, blue_p, red_p, red_p},
+				    {red_p, green_p, blue_p, blue_p},
+				    {blue_p, red_p, green_p, green_p},
+				    {red_p, blue_p, blue_p, blue_p},
+				    {red_p, green_p, green_p, green_p},
+				    {green_p, red_p, red_p, red_p},
+				    {green_p, blue_p, blue_p, blue_p},
+				    {blue_p, red_p, red_p, red_p}
 };
 
 int tile_number[MAX_DECK_SIZE] = {
-								  100, 100, 100, 100, 100,
-								  100, 100, 100, 100, 100,
-								  100, 100, 100, 100, 100,
-								  100, 100, 100, 100, 100, 								  
+				  100, 100, 100, 100, 100,
+				  100, 100, 100, 100, 100,
+				  100, 100, 100, 100, 100,
+				  100, 100, 100, 100, 100, 								  
 };
-
+*/
 // A function that fills a deck with tiles
 // The contents of the deck `d` after the call must always be the same.
 void deck_init(struct deck* d)
 {
-	for (int i = 0 ; i < MAX_DECK_SIZE ; ++i){
-		d->cards[i].t = tiles[i];
-		d->cards[i].n = tile_number[i];
-		d->size += tile_number[i];
-	}
+  // for (int i = 0 ; i < MAX_DECK_SIZE ; ++i){
+  // d->cards[i].t = tiles[i];
+  // d->cards[i].n = tile_number[i];
+  // d->size += tile_number[i];
+  //}
 }
 
