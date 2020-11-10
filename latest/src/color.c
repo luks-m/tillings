d143a8f8eb87f0cb7c6e247f13f87568d41ea92a
BOUDEAU Benjamin <Benjamin.Boudeau@bordeaux-inp.fr>
2020-11-10 14:50:57 +0100

    cation sur color.c et test.c

--- latest/src/color.c
+++ latest/src/color.c
@@ -12,14 +12,22 @@ struct color red = {"\e[0;31m","rouge"};
 struct color blue = {"\e[0;34m","bleu"};
 struct color green = {"\e[0;32m","vert"};
 
-struct color* colors[COLOR_NUMBER] = [&red, &blue, &green];
+struct color* colors[COLOR_NUMBER] = {&red, &blue, &green};
 
 const char* color_name(struct color* t){
-  return t->name;
+	for (int i = 0 ; i < COLOR_NUMBER ; ++i){
+		if (colors[i]->name == t->name)
+			return t->name;
+	}
+	return NULL;
 }
 
 const char* color_cstring(struct color* t) {
-  return t->ANSI;
+	for (int i = 0 ; i < COLOR_NUMBER ; ++i){
+		if (colors[i]->ANSI == t->ANSI)
+			return t->ANSI;
+	}
+	return NULL;
 }
 
 struct color* color_from_name(const char* name)
--- latest/tst/test.c
+++ latest/tst/test.c
@@ -1,4 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include "color.h"
+#include "tile.h"
+
 int main(int argc, char *argv[])
 {
-  return 0;
+	printf("Tests sur la structure color\n");
+	printf("Est ce que le programme vérifie bien qu'une couleur est valide avant d'en donner le nom ou le code ANSI ? ");
+	struct color invalide = {"testcode", "pasunecouleur"};
+	printf("la couleur correspondant à red est : %s\n", color_name(&red));
+	printf("la couleur correspondant à invalide est : %s\n", color_name(&invalide));
+	printf("le code ANSI correspondant à red est : %s\n", color_cstring(&red));
+	printf("la cde ANSI correspondant à invalide est : %s\n", color_cstring(&invalide));
+	printf("\n");
+	printf("On verifie que le pointeur retourné par color_from_name est valide");
+	struct color* bleu = color_from_name("bleu");
+	printf("la couleur correspondant à bleu est : %s", bleu->name);
+	printf("le code ANSI correspondant à bleu est : %s", bleu->ANSI);
+	return 0;
 }
