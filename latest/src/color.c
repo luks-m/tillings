a25b4080857b72062949be1c1e05c0a4f475ceda
BOUDEAU Benjamin <Benjamin.Boudeau@bordeaux-inp.fr>
2020-11-13 15:53:14 +0100

    finalisation du Makefile / modification des tests / modifications de color.c

--- latest/src/color.c
+++ latest/src/color.c
@@ -8,26 +8,25 @@ struct color{
   char *name;
 };
 
-struct color red = {"\e[0;31m","rouge"};
-struct color blue = {"\e[0;34m","bleu"};
-struct color green = {"\e[0;32m","vert"};
+struct color red = {"\e[0;31mrouge\033[0m","rouge"};
+struct color blue = {"\e[0;34mbleu\033[0m","bleu"};
+struct color green = {"\e[0;32mvert\033[0m","vert"};
 
 struct color* colors[COLOR_NUMBER] = {&red, &blue, &green};
 
-const char* color_name(struct color* t){
-	for (int i = 0 ; i < COLOR_NUMBER ; ++i){
-		if (colors[i]->name == t->name)
-			return t->name;
-	}
-	return NULL;
+const char* color_name(struct color* t)
+{
+	if (t == NULL)
+		return NULL;
+	return t->name;
+	
 }
 
-const char* color_cstring(struct color* t) {
-	for (int i = 0 ; i < COLOR_NUMBER ; ++i){
-		if (colors[i]->ANSI == t->ANSI)
-			return t->ANSI;
-	}
-	return NULL;
+const char* color_cstring(struct color* t)
+{
+	if (t == NULL)
+		return NULL;
+	return t->ANSI;
 }
 
 struct color* color_from_name(const char* name)
--- latest/tst/test.c
+++ latest/tst/test.c
@@ -5,17 +5,31 @@
 
 int main(int argc, char *argv[])
 {
-	printf("Tests sur la structure color\n");
-	printf("Est ce que le programme vérifie bien qu'une couleur est valide avant d'en donner le nom ou le code ANSI ? ");
-	struct color invalide = {"testcode", "pasunecouleur"};
-	printf("la couleur correspondant à red est : %s\n", color_name(&red));
-	printf("la couleur correspondant à invalide est : %s\n", color_name(&invalide));
-	printf("le code ANSI correspondant à red est : %s\n", color_cstring(&red));
-	printf("la cde ANSI correspondant à invalide est : %s\n", color_cstring(&invalide));
 	printf("\n");
-	printf("On verifie que le pointeur retourné par color_from_name est valide");
-	struct color* bleu = color_from_name("bleu");
-	printf("la couleur correspondant à bleu est : %s", bleu->name);
-	printf("le code ANSI correspondant à bleu est : %s", bleu->ANSI);
+
+	printf("Tests sur la structure color\n");
+	printf("Est ce que color_from_name / color_name / color_cstring fonctionnent ?\n"); 
+	printf("\n");
+	
+	printf("Voici 'rouge' écrit en rouge %s\n", color_cstring(color_from_name("rouge")));
+	printf("la couleur correspondant à red est : %s\n", color_name(color_from_name("rouge")));
+
+	printf("Voici 'bleu' écrit en bleu %s\n", color_cstring(color_from_name("bleu")));
+	printf("la couleur correspondant à blue est : %s\n", color_name(color_from_name("bleu")));
+
+	printf("Voici 'vert' écrit en vert %s\n", color_cstring(color_from_name("vert")));
+	printf("la couleur correspondant à green est : %s\n", color_name(color_from_name("vert")));
+	printf("\n");
+
+	printf("Tests sur la structure tile\n");
+	printf("Est ce que tile_is_empty / empty_tile fonctionnent ?\n");
+	printf("\n");
+
+	printf("Est ce qu'une tuile vide est vide : %s\n", tile_is_empty(empty_tile())?"oui":"non");
+	printf("\n");
+
+	printf("Est ce que tile_equals fonctionne avec des tuiles vides : %s\n",
+		   tile_equals(empty_tile(), empty_tile())?"oui":"non");
+	
 	return 0;
 }
