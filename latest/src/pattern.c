4cf09732dd6fdad46da66cdb0e3a41270b086012
MARAIS Lucas <Lucas.Marais@bordeaux-inp.fr>
2020-12-04 16:50:16 +0100

    modification de l'implémentation des patterns avec une structure (pointeur de fonction, score)

--- latest/src/pattern.c
+++ latest/src/pattern.c
@@ -4,7 +4,11 @@
 #include "pattern.h"
 #include "tile.h"
 
-int score_tab[] = {34, 20, 10};
+
+struct pattern_score {
+  int (*fun) (struct board_cell [MAX_SIZE_BOARD][MAX_SIZE_BOARD], int, int);
+  int score;
+};
 
 struct color* is_monochrome(const struct tile* t)
 {
@@ -30,8 +34,8 @@ int same_color(struct board_cell b[MAX_SIZE_BOARD][MAX_SIZE_BOARD], int x, int y
       if (tile_edge(tile(b, new_x, new_y), give_opposite(d1)) != c)
 	return 0;
     }
-      new_x = x + x_difference(d2);
-      new_y = y + y_difference(d2);
+    new_x = x + x_difference(d2);
+    new_y = y + y_difference(d2);
     if (new_x < 0 || new_x > MAX_SIZE_BOARD || new_y < 0 || new_y > MAX_SIZE_BOARD || tile(b, new_x, new_y) == NULL)
       return 0;
     else {
@@ -42,14 +46,73 @@ int same_color(struct board_cell b[MAX_SIZE_BOARD][MAX_SIZE_BOARD], int x, int y
   return 1;
 }
 
-int pattern(struct board_cell b[MAX_SIZE_BOARD][MAX_SIZE_BOARD], int x, int y)
+int pattern1(struct board_cell b[MAX_SIZE_BOARD][MAX_SIZE_BOARD], int x, int y)
 {
   if (same_color(b, x, y, NORTH, SOUTH) && same_color(b, x, y, WEST, EAST))
-    return score_tab[0];
-  if (same_color(b, x, y, NORTH, SOUTH) || same_color(b, x, y, WEST, EAST))
-    return score_tab[1];
-  if (same_color(b, x, y, NORTH, EAST) || same_color(b, x, y, NORTH, WEST) || same_color(b, x, y, SOUTH, WEST) || same_color(b, x, y, SOUTH, EAST))
-    return score_tab[2];
+    return 1;
   return 0;
 }
 
+int pattern2(struct board_cell b[MAX_SIZE_BOARD][MAX_SIZE_BOARD], int x, int y)
+{
+  if (same_color(b, x, y, NORTH, SOUTH))
+    return 1;
+  return 0;
+}
+
+int pattern3(struct board_cell b[MAX_SIZE_BOARD][MAX_SIZE_BOARD], int x, int y)
+{
+  if (same_color(b, x, y, WEST, EAST))
+    return 1;
+  return 0;
+}
+
+int pattern4(struct board_cell b[MAX_SIZE_BOARD][MAX_SIZE_BOARD], int x, int y)
+{
+  if (same_color(b, x, y, NORTH, EAST))
+    return 1;
+  return 0;
+}
+
+int pattern5(struct board_cell b[MAX_SIZE_BOARD][MAX_SIZE_BOARD], int x, int y)
+{
+  if (same_color(b, x, y, NORTH, WEST))
+    return 1;
+  return 0;
+}
+
+int pattern6(struct board_cell b[MAX_SIZE_BOARD][MAX_SIZE_BOARD], int x, int y)
+{
+  if (same_color(b, x, y, SOUTH, WEST))
+    return 1;
+  return 0;
+}
+
+int pattern7(struct board_cell b[MAX_SIZE_BOARD][MAX_SIZE_BOARD], int x, int y)
+{
+  if (same_color(b, x, y, SOUTH, EAST))
+    return 1;
+  return 0;
+}
+
+//Initialisation of the patterns and their score
+
+struct pattern_score patterns[NB_MOTIFS] = {
+					    {pattern1, 34},
+					    {pattern2, 20},
+					    {pattern3, 20},
+					    {pattern4, 10},
+					    {pattern5, 10},
+					    {pattern6, 10},
+					    {pattern7, 10}
+};
+
+int pattern(struct board_cell b[MAX_SIZE_BOARD][MAX_SIZE_BOARD], int x, int y)
+{ 
+  for (int i = 0; i < NB_MOTIFS; i++) {
+    if (patterns[i].fun(b, x, y))
+      return patterns[i].score;
+  }
+  
+  return 0;
+}
--- latest/src/pattern.h
+++ latest/src/pattern.h
@@ -3,7 +3,7 @@
 
 #include "board.h"
 
-#define MAX_MOTIFS 7
+#define NB_MOTIFS 7
 
 //return the color of the tile if it is monochrome
 struct color* is_monochrome(const struct tile* t);
@@ -11,7 +11,28 @@ struct color* is_monochrome(const struct tile* t);
 //test if the tile (x,y) and the tiles edges of the tiles in the direction d1 and d2 are same coloured
 int same_color(struct board_cell b[MAX_SIZE_BOARD][MAX_SIZE_BOARD], int x, int y, enum direction d1, enum direction d2);
 
-//return the score if the tile (x,y) is part of a pattern
+//test if there is a monochrome tile at position (x,y) with same color on others tiles in each direction
+int pattern1(struct board_cell b[MAX_SIZE_BOARD][MAX_SIZE_BOARD], int x, int y);
+
+//test if there is a monochrome tile at position (x,y) with same color on others tiles in NORTH and SOUTH directions
+int pattern2(struct board_cell b[MAX_SIZE_BOARD][MAX_SIZE_BOARD], int x, int y);
+
+//test if there is a monochrome tile at position (x,y) with same color on others tiles in WEST and EAST directions
+int pattern3(struct board_cell b[MAX_SIZE_BOARD][MAX_SIZE_BOARD], int x, int y);
+
+//test if there is a monochrome tile at position (x,y) with same color on others tiles in NORTH and EAST directions
+int pattern4(struct board_cell b[MAX_SIZE_BOARD][MAX_SIZE_BOARD], int x, int y);
+
+//test if there is a monochrome tile at position (x,y) with same color on others tiles in NORTH and WEST directions
+int pattern5(struct board_cell b[MAX_SIZE_BOARD][MAX_SIZE_BOARD], int x, int y);
+
+//test if there is a monochrome tile at position (x,y) with same color on others tiles in SOUTH and WEST directions
+int pattern6(struct board_cell b[MAX_SIZE_BOARD][MAX_SIZE_BOARD], int x, int y);
+
+//test if there is a monochrome tile at position (x,y) with same color on others tiles in SOUTH and EAST directions
+int pattern7(struct board_cell b[MAX_SIZE_BOARD][MAX_SIZE_BOARD], int x, int y);
+
+//return the score if the tile (x,y) is part of a pattern, testing all the patterns from the most important to the less importants
 int pattern(struct board_cell b[MAX_SIZE_BOARD][MAX_SIZE_BOARD], int x, int y);
 
 #endif //_PATTERN_H_
